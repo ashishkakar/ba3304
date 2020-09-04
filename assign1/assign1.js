e15914293c90fa55a0044d7ea1f30a0691754915
Ashish Kakar <ashish_kakar@hotmail.com>
2020-09-04 18:37:11 -0500

    simplified
    fixed input validation errors
    made logic simpler

--- assign1/assign1.js
+++ assign1/assign1.js
@@ -208,96 +208,281 @@ function generate_manufacturing_q() {
 
 /*HTML functions*/
 
-var element_keys = ["simp", "decoup", "initinv", "mdemand", "mcapacity", "sdemand", "scapacity", "sim"];
-var elements = {};
+/*Too complex*/
+/*
+var element_key_indexes = {};
+var element_keys = ["simp", "decoup", "initinv", "mdemand", "mcapacity",
+"sdemand", "scapacity", "sim"];
+var elements = [];
 var valid = [];
+var sections = ["instructions", "parameters", "simulation", "summary"];
 
 function param_init() {
-	elements["simp"].value = simulation_period;
-	elements["decoup"].checked = decouple;
-	elements["initinv"].value = initial_inventory;
-	if(decouple) {
-		elements["mdemand"].value = man_mean_demand;
-		elements["mcapacity"].value = man_capacity;
-		elements["sdemand"].value = serv_mean_demand;
-		elements["scapacity"].value = serv_capacity;
-	} else {
-		elements["mdemand"].value = mean_demand;
-		elements["sdemand"].value = mean_demand;
-		elements["mcapacity"].value = capacity;
-		elements["scapacity"].value = capacity;
-	}
-	elements["sim"].disabled = false;
+elements["simp"].value = simulation_period;
+elements["decoup"].checked = decouple;
+elements["initinv"].value = initial_inventory;
+if(decouple) {
+elements["mdemand"].value = man_mean_demand;
+elements["mcapacity"].value = man_capacity;
+elements["sdemand"].value = serv_mean_demand;
+elements["scapacity"].value = serv_capacity;
+} else {
+elements["mdemand"].value = mean_demand;
+elements["sdemand"].value = mean_demand;
+elements["mcapacity"].value = capacity;
+elements["scapacity"].value = capacity;
+}
+elements["sim"].disabled = false;
 }
 
 function load() {
-	for(key of element_keys) elements[key] = document.getElementById(key);
-	for(key of element_keys) valid.push(true);
-	param_init();
+var i=0;
+for(key of element_keys) {
+element_key_indexes["key"] = i;
+elements.push(document.getElementById(key));
+i++;
+}
+for(; i>=0; i--) valid.push(true);
+sections["parameters"] = document.getElementById("parameters");
+sections["simulation"] = document.getElementById("simulation");
+param_init();
 }
 
 function bad_input(key) {
-	elements["sim"].disabled = true;
-	valid[element_keys.indexOf(key)] = false;
-	elements[key].classList.toggle("error");
+elements["sim"].disabled = true;
+elements[key].classList.add("error");
+valid[element_keys.indexOf(key)] = false;
 }
 
 function good_input(key) {
-	valid[element_keys.indexOf(key)] = true;
-	for(v of valid) if(!v) return;
-	elements["sim"].disabled = false;
-	elements[key].classList.toggle("error");
+valid[element_keys.indexOf(key)] = true;
+elements[key].classList.remove("error");
+for(v of valid) if(!v) return;
+elements["sim"].disabled = false;
 }
 
 function check_whole(x, key) {
+var val=0;
+if(isNaN(x)) {
+bad_input(key);
+return false;
+}
+if(Number.isInteger(parseFloat(x))) {
+val = parseInt(x);
+if(val >= 0) {
+good_input(key);
+return true;
+}
+}
+bad_input(key);
+return false;
+}
+
+function check_natural(x, key) {
+var val=0;
+if(isNaN(x)) {
+bad_input(key);
+return false;
+}
+if(Number.isInteger(parseFloat(x))) {
+val = parseInt(x);
+if(val > 0) {
+good_input(key);
+return true;
+}
+}
+bad_input(key);
+return false;
+}
+
+function check_floatgt0(x, key) {
+if(isNaN(x)) {
+bad_input(key);
+return false;
+}
+if(parseFloat(x)>0) {
+good_input(key);
+return true;
+}
+bad_input(key);
+return false;
+}
+
+function simp(x) {
+if(check_natural(x.value, "simp")) simulation_period = parseInt(x.value);
+}
+
+function decoup(x) {
+decouple = x.checked;
+}
+
+function mdemand(x) {
+var val = 0;
+if(check_floatgt0(x.value, "mdemand")) {
+val = parseFloat(x.value);
+if(decouple)
+man_mean_demand = val;
+else {
+mean_demand = val;
+man_mean_demand = val;
+serv_mean_demand = val;
+elements["sdemand"].value = val;
+good_input("sdemand");
+}
+}
+}
+
+function mcapacity(x) {
+var val = 0;
+if(check_natural(x.value, "mcapacity")) {
+val = parseInt(x.value);
+if(decouple)
+man_capacity = val;
+else {
+capacity = val;
+man_capacity = val;
+serv_capacity = val;
+elements["scapacity"].value = val;
+good_input("scapacity");
+}
+}
+}
+
+function invent(x) {
+if(check_whole(x.value, "initinv")) initial_inventory = parseInt(x.value);
+}
+
+function sdemand(x) {
+var val = 0;
+if(check_floatgt0(x.value, "sdemand")) {
+val = parseFloat(x.value);
+if(decouple)
+serv_mean_demand = val;
+else {
+mean_demand = val;
+man_mean_demand = val;
+serv_mean_demand = val;
+elements["mdemand"].value = val;
+good_input("mdemand");
+}
+}
+}
+
+function scapacity(x) {
+var val = 0;
+if(check_natural(x.value, "scapacity")) {
+val = parseInt(x.value);
+if(decouple)
+serv_capacity = val;
+else {
+capacity = val;
+man_capacity = val;
+serv_capacity = val;
+elements["mcapacity"].value = val;
+good_input("mcapacity");
+}
+}
+}
+*/
+
+/*Simpler scheme*/
+var element_keys = ["simp", "decoup", "initinv", "mdemand", "mcapacity",
+"sdemand", "scapacity", "sim", "time", "demand", "squeue", "mqueue",
+"inventory", "next", "end", "demandchart", "queuechart", "inventorychart"];
+var elements = [];
+var section_keys = ["instructions", "parameters", "simulation", "summary"];
+var sections = [];
+var valid = [];
+
+function param_init() {
+	elements[0].value = simulation_period;
+	elements[1].checked = decouple;
+	elements[2].value = initial_inventory;
+	if(decouple) {
+		elements[3].value = man_mean_demand;
+		elements[4].value = man_capacity;
+		elements[5].value = serv_mean_demand;
+		elements[6].value = serv_capacity;
+	} else {
+		elements[3].value = mean_demand;
+		elements[4].value = capacity;
+		elements[5].value = mean_demand;
+		elements[6].value = capacity;
+	}
+	elements[7].disabled = false;
+}
+
+function load() {
+	for(key of element_keys) {
+		elements.push(document.getElementById(key));
+		valid.push(true);
+	}
+	for(key of section_keys) sections.push(document.getElementById(key));
+	param_init();
+}
+
+function bad_input(i) {
+	elements[7].disabled = true;
+	elements[i].classList.add("error");
+	valid[i] = false;
+}
+
+function good_input(i) {
+	valid[i] = true;
+	elements[i].classList.remove("error");
+	for(v of valid) if(!v) return;
+	elements[7].disabled = false;
+}
+
+function check_whole(x, i) {
 	var val=0;
 	if(isNaN(x)) {
-		bad_input(key);
+		bad_input(i);
 		return false;
 	}
 	if(Number.isInteger(parseFloat(x))) {
 		val = parseInt(x);
 		if(val >= 0) {
-			good_input(key);
+			good_input(i);
 			return true;
 		}
 	}
-	bad_input(key);
+	bad_input(i);
 	return false;
 }
 
-function check_natural(x, key) {
+function check_natural(x, i) {
 	var val=0;
 	if(isNaN(x)) {
-		bad_input(key);
+		bad_input(i);
 		return false;
 	}
 	if(Number.isInteger(parseFloat(x))) {
 		val = parseInt(x);
 		if(val > 0) {
-			good_input(key);
+			good_input(i);
 			return true;
 		}
 	}
-	bad_input(key);
+	bad_input(i);
 	return false;
 }
 
-function check_floatgt0(x, key) {
+function check_floatgt0(x, i) {
 	if(isNaN(x)) {
 		bad_input(key);
 		return false;
 	}
 	if(parseFloat(x)>0) {
-		good_input(key);
+		good_input(i);
 		return true;
 	}
-	bad_input(key);
+	bad_input(i);
 	return false;
 }
 
 function simp(x) {
-	if(check_natural(x.value, "simp")) simulation_period = parseInt(x.value);
+	if(check_natural(x.value, 0)) simulation_period = parseInt(x.value);
 }
 
 function decoup(x) {
@@ -306,7 +491,7 @@ function decoup(x) {
 
 function mdemand(x) {
 	var val = 0;
-	if(check_floatgt0(x.value, "mdemand")) {
+	if(check_floatgt0(x.value, 3)) {
 		val = parseFloat(x.value);
 		if(decouple)
 		man_mean_demand = val;
@@ -314,15 +499,15 @@ function mdemand(x) {
 			mean_demand = val;
 			man_mean_demand = val;
 			serv_mean_demand = val;
-			elements["sdemand"].value = val;
-			good_input("sdemand");
+			elements[5].value = val;
+			good_input(5);
 		}
 	}
 }
 
 function mcapacity(x) {
 	var val = 0;
-	if(check_natural(x.value, "mcapacity")) {
+	if(check_natural(x.value, 4)) {
 		val = parseInt(x.value);
 		if(decouple)
 		man_capacity = val;
@@ -330,19 +515,19 @@ function mcapacity(x) {
 			capacity = val;
 			man_capacity = val;
 			serv_capacity = val;
-			elements["scapacity"].value = val;
-			good_input("scapacity");
+			elements[6].value = val;
+			good_input(6);
 		}
 	}
 }
 
 function invent(x) {
-	if(check_whole(x.value, "initinv")) initial_inventory = parseInt(x.value);
+	if(check_whole(x.value, 2)) initial_inventory = parseInt(x.value);
 }
 
 function sdemand(x) {
 	var val = 0;
-	if(check_floatgt0(x.value, "sdemand")) {
+	if(check_floatgt0(x.value, 5)) {
 		val = parseFloat(x.value);
 		if(decouple)
 		serv_mean_demand = val;
@@ -350,15 +535,15 @@ function sdemand(x) {
 			mean_demand = val;
 			man_mean_demand = val;
 			serv_mean_demand = val;
-			elements["mdemand"].value = val;
-			good_input("mdemand");
+			elements[3].value = val;
+			good_input(3);
 		}
 	}
 }
 
 function scapacity(x) {
 	var val = 0;
-	if(check_natural(x.value, "scapacity")) {
+	if(check_natural(x.value, 6)) {
 		val = parseInt(x.value);
 		if(decouple)
 		serv_capacity = val;
@@ -366,15 +551,33 @@ function scapacity(x) {
 			capacity = val;
 			man_capacity = val;
 			serv_capacity = val;
-			elements["mcapacity"].value = val;
-			good_input("mcapacity");
+			elements[4].value = val;
+			good_input(4);
 		}
 	}
 }
 
 function simulate(x) {
+	x.disabled = true;
+	sections[1].classList.add("remove");
+	sections[2].classList.remove("remove");
 	init();
 	generate_demand();
 	generate_service_q();
 	generate_manufacturing_q();
+}
+
+
+/*Simulation*/
+
+var time=0;
+
+function nextdraw(x) {
+	time++;
+	elements[8].innerText = time;
+	elements[9].innerText =
+	if(time>=simulation_period) {
+		x.disabled=true;
+		elements[14].disbaled=true;
+	}
 }
